64d3d1c4a79538473104713a9057a52492580fde
Alan Etkin <spametki@gmail.com>
2011-09-01 13:28:13 -0300

    Relocate operations.

--- controllers/crm.py
+++ controllers/crm.py
@@ -1,6 +1,48 @@
 # coding: utf8
 # intente algo como
+
+import datetime
+
 def index(): return dict(message="hello from crm.py")
+
+@auth.requires_login()
+def customer_panel():
+    """ Customer on-line panel. Show info/stats/links to actions"""
+    contact_user = db(db.contact_user.user_id == auth.user_id).select().first()
+    if contact_user is None:
+        return dict(customer_orders = None, message="No tax id selected", customer = None)
+    try:
+        contact = db(db.contact.contact_id == contact_user.contact_id).select().first()
+    except KeyError:
+        contact = None
+    try:
+        customer = db(db.customer.customer_id == contact.customer_id).select().first()
+    except KeyError:
+        customer = None
+    
+    now = datetime.datetime.now()
+    delta = datetime.timedelta(7)
+    time_limit = now - delta
+
+    q = db.operation.document_id == db.document.document_id
+    q &= db.document.orders == True
+    preset = db(q)
+    customer_orders_set = preset((db.operation.customer_id == customer) & (db.operation.posted >= time_limit))
+    
+    """
+    TODO: filter by order document type
+
+    # list all document types with orders == True
+    order_documents = db(db.document.orders == True).select()
+    order_documents_list = [document.document_id for document in order_documents]    
+    # make a subset of operations with order documents
+    customer_orders_subset = customer_orders_set & db()
+    """
+    # get operation rows
+    customer_orders = customer_orders_set.select()
+
+    return dict(customer_orders = customer_orders, message="Customer panel", customer = customer)
+
 
 def current_account_report():
     """ Performs a query of operations and
@@ -50,3 +92,12 @@ def current_account_report():
         headers=headers, linkto=URL(c="operations", f="movements"))
     return dict(query_form = query_form, operations = operations, \
     total_debt = total_debt)
+
+
+def new_customer():
+    form = crud.create(db.customer)
+    return dict(form = form)
+
+def new_subcustomer():
+    form = crud.create(db.subcustomer)
+    return dict(form = form)
